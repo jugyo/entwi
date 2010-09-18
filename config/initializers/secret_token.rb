00062ed338b7ee5c4901afced03f98100592ddef
jugyo <jugyo.org@gmail.com>
2010-09-18 18:12:52 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Entwi::Application.config.secret_token = '859b4c2782d38c7dae14b22f4212928085b3daea682345608b834dc1b2698216f4703c8715de9de24d910d77ba3936b0e0aa9b7ac6c66059c1f663d3b7496934'
