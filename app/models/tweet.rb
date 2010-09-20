ba1cdf16cf4805bb58fb1c98f38d8afaa025c1a6
jugyo <jugyo.org@gmail.com>
2010-09-20 23:36:17 +0900

    tweet に翻訳の種別をもたせるようにした

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,13 @@
+# coding: utf-8
 class Tweet
+  # TODO: i18n
+  TRANS_TYPES = {'en2ja' => '英→日', 'ja2en' => '日→英'}
+
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :text
+  field :trans_type
 
   referenced_in :user
   references_many :comments
