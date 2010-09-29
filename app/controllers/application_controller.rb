0d0f453140af1b4bab6c705ee0cd910b87c98c94
jugyo <jugyo.org@gmail.com>
2010-09-30 01:54:14 +0900

    ログイン／ログアウト後に同じページに戻ってくるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter do
+    if params[:return_to].present?
+      session[:return_to] = params[:return_to]
+    end
+  end
 end
